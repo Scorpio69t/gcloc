f461fd4587a3e3b0f2cd5439929e972bb8166439
Mr Yang <18728163854@163.com>
2024-11-14 10:27:06 +0800

    language test

--- pkg/language/language_test.go
+++ pkg/language/language_test.go
@@ -18,3 +18,131 @@ func TestLoadFileExts(t *testing.T) {
 
 	t.Logf("file extensions count: %d", len(exts))
 }
+
+func TestGetShebang(t *testing.T) {
+	lang := "py"
+	shebang := "#!/usr/bin/env python"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangWithSpace(t *testing.T) {
+	lang := "py"
+	shebang := "#! /usr/bin/env python"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangBashWithEnv(t *testing.T) {
+	lang := "bash"
+	shebang := "#!/usr/bin/env bash"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangBash(t *testing.T) {
+	lang := "bash"
+	shebang := "#!/usr/bin/bash"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangBashWithSpace(t *testing.T) {
+	lang := "bash"
+	shebang := "#! /usr/bin/bash"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangPlan9Shell(t *testing.T) {
+	lang := "plan9sh"
+	shebang := "#!/usr/rc"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangStartDot(t *testing.T) {
+	lang := "pl"
+	shebang := "#!./perl -o"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
+
+func TestGetShebangMk(t *testing.T) {
+	lang := "make"
+	shebang := "#!/usr/bin/make -f"
+
+	s, ok := GetShebang(shebang)
+	if !ok {
+		t.Errorf("invalid logic. shebang=[%v]", shebang)
+	}
+
+	if lang != s {
+		t.Errorf("invalid logic. lang=[%v] shebang=[%v]", lang, s)
+	}
+
+	t.Logf("lang=[%v] shebang=[%v]", lang, s)
+}
