f786a48e79097bced9448e1b1d17661036ce5c80
Mr Yang <18728163854@163.com>
2024-11-14 16:40:43 +0800

    json test

---
+++ pkg/json/json_test.go
@@ -0,0 +1,43 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"gcloc/pkg/file"
+	"gcloc/pkg/language"
+	"testing"
+)
+
+func TestOutputJSON(t *testing.T) {
+	total := &language.Language{}
+	files := []file.GClocFile{
+		{Name: "one.go", Language: "Go"},
+		{Name: "two.go", Language: "Go"},
+	}
+
+	jsonResult := NewFilesResultFromGCloc(total, files)
+	if jsonResult.Files[0].Name != "one.go" {
+		t.Errorf("invalid result. Name: one.go")
+	}
+
+	if jsonResult.Files[1].Name != "two.go" {
+		t.Errorf("invalid result. Name: two.go")
+	}
+
+	if jsonResult.Files[1].Language != "Go" {
+		t.Errorf("invalid result. lang: Go")
+	}
+
+	// check output json text
+	buf, err := json.Marshal(jsonResult)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("json marshal error")
+	}
+
+	actualJSONText := `{"files":[{"name":"one.go","language":"Go","codes":0,"comments":0,"blanks":0},{"name":"two.go","language":"Go","codes":0,"comments":0,"blanks":0}],"total":{"file_count":0,"codes":0,"comments":0,"blanks":0}}`
+	resultJSONText := string(buf)
+	if actualJSONText != resultJSONText {
+		t.Errorf("invalid result. '%s'", resultJSONText)
+	}
+}
