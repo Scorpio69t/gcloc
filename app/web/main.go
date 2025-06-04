74f276f48133f61cfae8a2cd5d9445bdef8e5e7a
yangruitao <18728163854@163.com>
2025-06-04 09:23:10 +0800

    use cmd web

--- app/web/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/Scorpio69t/gcloc/web"
-
-func main() {
-	if err := web.Start(":8080"); err != nil {
-		panic(err)
-	}
-}
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/Scorpio69t/gcloc
 
-go 1.20
+go 1.22
+
+toolchain go1.24.0
 
 require (
 	github.com/go-enry/go-enry/v2 v2.9.1
